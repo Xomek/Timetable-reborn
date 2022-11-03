425bf37db4e229ea0565cb3dd738f465d7267ca3
XomekX <z_v_m_x@mail.ru>
2022-11-03 16:52:45 +0700

    create Input component

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -1,7 +1,32 @@
-import { FC } from "react";
+import { COLORS } from "enums/colors.enum";
+import { ChangeEvent, FC, InputHTMLAttributes } from "react";
+import cn from "classnames";
+import styles from "./Input.module.css";
 
-const Input: FC = () => {
-  return <input type="text" />;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  color?: COLORS;
+}
+
+const Input: FC<InputProps> = ({
+  type = "text",
+  label,
+  onChange,
+  color,
+  ...props
+}) => {
+  return (
+    <label className={styles.box}>
+      <input
+        className={cn(styles.input, color && { [styles[color]]: color })}
+        type={type}
+        onChange={(e) => onChange && onChange(e)}
+        {...props}
+      />
+      {label && <div className={styles.label}>{label}</div>}
+    </label>
+  );
 };
 
 export default Input;
